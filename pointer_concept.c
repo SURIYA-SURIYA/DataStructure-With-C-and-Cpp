e32815ccc47ec48f0ac5b51fbabba35f1abc3080
Suriya <155054111+SURIYA-SURIYA@users.noreply.github.com>
2024-02-12 22:37:07 +0530

    Add files via upload

---
+++ pointer_concept.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function  
+    int x,y;
+    x=*a+*b;
+    y =*a-*b;
+    //printf("%d",x);
+    //printf("%d",y);
+    *a = x;
+    *b =abs(y); 
+    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
